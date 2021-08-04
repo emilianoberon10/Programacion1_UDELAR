fb0aa15e0631e726752b1bac8e6acf6cda4ee647
emilianoberon10 <emilianoberon10@gmail.com>
2021-08-04 16:18:13 -0300

    first commit

---
+++ p1.pas
@@ -0,0 +1,15 @@
+Program HelloWorld;
+var a,b,c,d,x,y,z : integer;
+Begin
+
+readln (x,y,z);
+readln (a);
+readln (b,c);
+read (d) ;
+
+writeln(x,y,z);
+writeln(a);
+writeln(b,c);
+writeln(d);
+
+End.
